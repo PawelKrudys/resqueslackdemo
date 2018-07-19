47dc86f27608b054ff4fb9a3beac6a310e874c69
Pawel Krudys <pawel.krudys@anixe.pl>
2018-07-19 08:34:36 +0200

    Add slack communication

--- Gemfile
+++ Gemfile
@@ -15,7 +15,10 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
-
+gem 'http'
+gem 'json'
+gem 'faye-websocket'
+gem 'eventmachine'
 gem 'resque'
 gem 'resque-web', '~> 0.0.7', require: 'resque_web'
 # Use CoffeeScript for .coffee assets and views
--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,18 +1,45 @@
+#require 'http'
+require 'json'
+
+
+TOKEN = 'xoxb-346632953504-396440310195-BzvJty0BeviBkeBIrqfljkBt'
+
 class TestController < ApplicationController
 
 class Foo
 	@queue = "cokolwiek"
 	def self.perform(args)
 		puts args
-		puts "bangla"
-		raise "błąd"
+		if args == "Fail" 
+				puts "Job failed"
+				HTTP.post("https://slack.com/api/chat.postMessage", params:{
+				token: TOKEN,
+				channel: '#test',
+				text: "*Job failed with arguments:* \n #{args}",
+				as_user: true
+				})
+				raise "Request not available"
+			else 
+				puts "Job succedeed"
+		end
 	end
 end 
 
-def test
-Resque.enqueue(Foo, "Pawel")
+def pierwszy
+Resque.enqueue(Foo, "Fail")
+#Resque.enqueue(Foo, "Jest OK")
 render plain: "Testowy string"
 end
 
+def drugi
+#Resque.enqueue(Foo, "Fail")
+Resque.enqueue(Foo, "Jest OK")
+render plain: "Inny string"
+end
+
 end 
+
+
+
+
 
